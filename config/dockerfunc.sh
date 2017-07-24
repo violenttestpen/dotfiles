8f1fa9afab1e991a9ef700400c0a291b686f5b55
Winston Ho <winstonho90@users.noreply.github.com>
2017-07-24 21:23:36 +0800

    Added dockerfunc

---
+++ config/dockerfunc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+LGRAY='\033[0;37m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+FILENAME='dockerfunc'
+NEW_FILENAME='.dockerfunc'
+
+SRC_PATH="dotfiles/${FILENAME}"
+DEST_PATH="${HOME}/${NEW_FILENAME}"
+
+echo -e "${LGRA}[*] Executing ${FILENAME}.sh...${NC}"
+
+cat $SRC_PATH > $DEST_PATH
+
+echo
+[ $? -eq 0 ] && echo -e "${GREEN}[+] Execution is successful!${NC}" || echo -e "${RED}[-] Execution failed.${NC}"
+echo
