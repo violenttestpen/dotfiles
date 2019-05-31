f709946fbb8c46c744cefc7831d4abd7f153409e
Winston Ho <winstonho90@noreply.user.github.com>
2019-05-31 08:05:22 +0800

    Added install_python_essentials.sh

---
+++ config/install_python_essentials.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Package Management
+pip3 --no-cache-dir install \
+    pipdeptree \
+    pipenv
+
+# HTTP Utilities
+pip3 --no-cache-dir install \
+    splinter \
+    httpie
+
+# Productivity
+pip3 --no-cache-dir install \
+    konch \
+    isort \
+    flake8
